d1a40e6dc0e9c07182860a83bae4bb8173f25d99
Developer <119800456+AloneXBots@users.noreply.github.com>
2023-03-04 19:02:15 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -241,7 +241,7 @@ Type /{BotCommands.HelpCommand} to get a list of available commands
         else:
             sendMarkup(start_string, context.bot, update.message, reply_markup)
     else:
-        text = f"Not Authorized user, deploy your own mirror bot"
+        text = f"<b>Ok Vro 🌝 /nFile Now Send On Your PM !</b>"
         if PICS:
             sendPhoto(text, context.bot, update.message, random.choice(PICS), reply_markup)
         else:
